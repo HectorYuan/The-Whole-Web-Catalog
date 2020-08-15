a1c4abfcefbd52f81abdd7686edd38333ba68972
yeonix <yuanyedmc@hotmail.com>
2020-08-15 19:09:54 +0800

    Mode Changed

--- csv2md.py
+++ csv2md.py
@@ -7,11 +7,12 @@ def csv2df(file_path):
 
     '''将带有网址信息的csv文件进行初步处理，并转换为dataframe对象'''
     columns = ['名称', '介绍', '地址', '场景', '形态', '推荐','标签', 
-        '网站质量', '访问方式', '适用人群','排名']
+        '网站质量', '访问方式','排名']
 
     df = pd.read_csv(file_path,usecols=columns)
-    df['名称'] =  df.apply(lambda x: '['+ str(x['名称']) + '](' + str(x['地址'])+')', axis=1)
-    df = df.sort_values(['排名','适用人群','访问方式'],ascending=True).drop(columns=['排名','地址'])
+    df['名称'] =  df.apply(lambda x: '['+ str(x['名称']) + '](' + str(x['地址'])+r') | `'+ str(x['推荐']) + r'` | `' + str(x['网站质量'])+'`', axis=1)
+    df['标签'] = df.apply(lambda x: str(x['标签']) +', '+ str(x['访问方式']) ,axis=1)
+    df = df.sort_values(['排名','访问方式'],ascending=True).drop(columns=['排名','地址','推荐','网站质量','访问方式'])
 
     return df
 
@@ -23,7 +24,6 @@ def df2dict(df, string):
     dfs = {key:df[df[string] == key].drop(columns=[string]) for key in df_info.keys()}
 
     for value in dfs.values():
-        value = value.sort_values(by =['网站质量','适用人群','访问方式'],ascending = [False,True,True])
         value = value.to_dict('records')
 
     dfs_dict = {key: value.to_dict('records') for key, value in dfs.items()}
@@ -45,42 +45,38 @@ def main_writer(file_path):
     extend_infos = {key: df2dict(value, '形态')[2] for key,value in base_dfs.items()}
 
     with open(f'{file_name}.md','w', encoding='utf-8') as f:
+        f.write('# 互联网漫游指南\n\n')
         f.write('{:toc}\n\n')
         f.write(f'## 场景概况\n\n')
         for key, value in base_info.items():
-            f.write(f'> {key} ---- {value}\n\n')
+            f.write(f'> [{key}]({key}场景汇总.md) --- 共计 {value}\n\n')
 
-        def one_layer(dicts = base_dfs_dict):
-            for key, value in dicts.items():
-                f.write(f'\n## {key}\n\n')
-                for dict in value:
-                    for key, value in dict.items():
-                        if key == '名称':
-                            f.write(f'\n\n### {value}\n')
-                        else:
-                            f.write(f'\n{key}: {value}\n')
-                    f.write(f'\n---')
-
-        def two_layer(dicts = extend_dicts):
-            for key_1, dictsets in dicts.items():
-                f.write(f'\n## {key_1}\n\n')
-                f.write(f'\n### 类别概况\n\n')
+    def two_layer(dicts = extend_dicts):
+        for key_1, dictsets in dicts.items():
+            with open(f'{key_1}场景汇总.md','w',encoding='utf-8') as f:
+                f.write(f'# {key_1}场景网站列表\n\n')
+                f.write(f'\n## 类别概况\n\n')
                 for info_key,info_value in extend_infos[key_1].items():
-                    f.write(f'> {info_key} ---- {info_value}\n\n')
+                    f.write(f'> {info_key} --- 共计 {info_value}\n\n')
                 for key_2, value_2 in dictsets.items():
-                    f.write(f'\n### {key_2}\n\n')
+                    f.write(f'\n## {key_2}类\n\n')
                     for dict in value_2:
                         for key, value in dict.items():
                             if key == '名称':
-                                f.write(f'\n\n#### {value}\n')
+                                f.write(f'\n\n### {value}\n')
+                            elif key == '介绍':
+                                f.write(f'\n> {value}\n')
+                            elif key == '标签':
+                                list = [f'`{i}`' for i in value.split(', ')]
+                                value = '  '.join(list)
+                                f.write(f'\n主要特性: {value}\n')
                             else:
                                 f.write(f'\n{key}: {value}\n')
                         f.write(f'\n---')
 
-        two_layer()               
+    two_layer()           
 
     return extend_dicts, extend_infos
 
-#%%
 file_path = r'互联网漫游指南 1e6e510f75aa4cd9a80191e68fccccff.csv'
 extend_dicts, extend_infos = main_writer(file_path)
